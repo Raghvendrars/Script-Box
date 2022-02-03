6e2221af0da0e50654dbfc9f3bdcb605e17a8998
Raghvendra Singh <Raghav26897@gmail.com>
2022-02-03 10:09:50 +0530

    up

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -10,6 +10,10 @@ import {
   Stack,
   Button,
   Paper,
+  Divider,
+  List,
+  ListItem,
+  ListItemText,
 } from "@mui/material";
 import HeroImg from "./Character.png";
 import "./Home.css";
@@ -23,54 +27,51 @@ const Home = () => {
     color: theme.palette.text.secondary,
   }));
 
+  const style = {
+    width: "100%",
+    maxWidth: "100vw",
+    bgcolor: "background.paper",
+  };
+
   return (
     <>
       <Grid container>
-        <Grid item xl={6} lg={5} md={12} sm={12} xs={12}>
+        <Grid item xl={6} lg={6} md={5} sm={12} xs={12}>
           <Box className="Hero-Section-Column-One-Box">
             <Grid
               container
               spacing={0}
-              direction="column"
+              // direction="column"
               alignItems="center"
               justifyContent="center"
-              style={{ minHeight: "100vh" }}
+              style={{ minHeight: "100vh",width:"80%" }}
               className="Hero-Section-Column-One-Box-Container"
             >
-              <Stack spacing={2} className="Hero-Section-Column-One-Box-Item">
-                <Item
-                  className="Hero-Section-Column-One-Box-Item"
-                  sx={{ boxShadow: "none",height:"50vh" }}
-                >
-                  <p className="Hero-Section-Text">
-                    We boost growth for your statup business
-                  </p>
-                </Item>
-                <Item
-                  className="Hero-Section-Column-One-Box-Item"
-                  sx={{ boxShadow: "none" }}
-                >
-                  <p className="Hero-Section-Text-Small">
-                    Our goal is top at the heart of creativity services industry
-                    as a digital creator. In has a after comment
-                  </p>
-                </Item>
-                <Item
-                  className="Hero-Section-Column-One-Box-Item"
-                  sx={{ textAlign: "left", boxShadow: "none" }}
-                >
+              <List sx={style} >
+                <ListItem>
+                  <div>
+                    <h1 className="Hero-Section-Text">
+                      We boost growth for your statup business
+                    </h1>
+                    <p className="Hero-Section-Text-Small">
+                      Our goal is top at the heart of creativity services
+                      industry as a digital creator. In has a after comment
+                    </p>
+                  </div>
+                </ListItem>
+                <ListItem>
                   <Button
                     className="Hero-Section-GetStarted-Button"
                     sx={{ bgcolor: "#ff6600", color: "#ffffff" }}
                   >
                     Get Started
                   </Button>
-                </Item>
-              </Stack>
+                </ListItem>
+              </List>
             </Grid>
           </Box>
         </Grid>
-        <Grid item xl={6} lg={7} md={12} sm={12} xs={12}>
+        <Grid item xl={6} lg={6} md={7} sm={12} xs={12}>
           <Grid
             container
             spacing={0}
@@ -80,8 +81,8 @@ const Home = () => {
             style={{ minHeight: "100vh" }}
             sx={{ boxShadow: "none" }}
           >
-            <Card sx={{ width: "100%", boxShadow: "none" }}>
-              <img src={HeroImg} alt="HeroSecImg" />
+            <Card sx={{ maxWidth: "100%", boxShadow: "none" }}>
+              <img src={HeroImg} alt="HeroSecImg" width="90%" />
             </Card>
           </Grid>
         </Grid>
